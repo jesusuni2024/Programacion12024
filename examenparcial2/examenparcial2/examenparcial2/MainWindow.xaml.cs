f1ef2623fde2667fbcb1c48d9d08248f7c50ec2d
jesusuni2024 <160173574+jesusuni2024@users.noreply.github.com>
2024-04-27 10:28:25 -0600

    Add files via upload

---
+++ examenparcial2/examenparcial2/examenparcial2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+class Calculadora 
+
+{
+    static void Main (string[] args)
+
+    {
+        while (true)
+        {
+            Console.WriteLine("Menu de calculadora");
+            Console.WriteLine("1. sum");
+            Console.WriteLine("Resta");
+            Console.WriteLine("Multiplicacion");
+            Console.WriteLine("Division");
+            Console.WriteLine("Raiz cuadrada");
+            Console.WriteLine("cos");
+            Console.WriteLine("Sen");
+            Console.WriteLine("Tan");
+
+            Console.Write("Selecciona la opcion");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            double result = 0; 
+            switch (choice)
+            {
+                case 1:
+                    result = Sum();
+                    break; 
+                case 2:
+                    result = Subtract();
+                    break;
+                case 3: 
+                    result = IMultiplyOperators ()
+                    break;
+                case 4: 
+                    result = Divide();
+                    break;
+                case 5:
+                    result = IPowerFunctions();
+
+                     
+
+            }
+}
